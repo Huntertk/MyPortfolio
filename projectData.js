3135e84211afd1e640ad04cd486e5ee6b4fbf81e
MD TAUFIK <110840076+Huntertk@users.noreply.github.com>
2023-05-19 23:01:59 +0530

    Eleven Commit In this commit adding some projects to the project page and Fixing UI Bugs and hosting image on other website to call the image uin project by url and UI created only for Mobile

---
+++ projectData.js
@@ -0,0 +1,22 @@
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+export default [
+    {
+        id: uuidv4(),
+        name: "Anonymously Tweet",
+        imageUrl: 'https://i.postimg.cc/gkWttkvK/project-1.png',
+        githubUrl: 'https://github.com/Huntertk/Anonymously-Tweet',
+        hostingUrl: 'https://react-anonymously-tweet-app-taufik.netlify.app',
+        details: "This Anonymously Tweet is a real time free Anonymously tweet service this has ability to save and store tweet without any personal details.",
+        language: ["react","css","firebase"]
+    },
+
+    {
+        id: uuidv4(),
+        name: "Add To Cart",
+        imageUrl: 'https://i.postimg.cc/Z0td4xkD/project-3.png',
+        githubUrl: 'https://github.com/Huntertk/AddToCardMobile',
+        hostingUrl: 'https://app-add-cart-item-taufik.netlify.app',
+        details: "It is a Todos with Integrated With Database that can able to data store. ",
+        language: ["HTML","css","firebase", "JavaScript"]
+    },
+]
--- src/Pages/Work.jsx
+++ src/Pages/Work.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import projectData from '../../projectData.js'
+import { BsGithub, BsBrowserChrome } from "react-icons/bs";
+import './work.css'
+import Aos from 'aos';
+import 'aos/dist/aos.css'
+
+
+
 
 const Work = () => {
+
+  useEffect(() => {
+    Aos.init()
+  },[])
+
+  const renderProject = projectData.map((project) => {
+    return <div className="work-project" key={project.id}
+    data-aos="fade-up"
+     data-aos-anchor-placement="top-bottom"
+    >
+        <img src={project.imageUrl} alt="image" className='work-project-image'/>
+        <h1>{project.name}</h1>
+        <p>{project.details}</p>
+        <div className="work-project-language">
+            {project.language.map((lang, index) => {
+              return <span key={index}>{lang}</span>
+            })}
+        </div>
+        <div className="work-icon">
+          <a href={project.githubUrl} target='_blank'><BsGithub /></a>
+          <a href={project.hostingUrl} target='_blank'><BsBrowserChrome /></a>
+          
+        </div>
+      </div>
+  })
+  console.log(projectData);
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='work-main-container'>
+      <h1>All Projects</h1>
+      {renderProject}
     </div>
   )
 }
